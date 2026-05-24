5c958f3a3af5609de96c089047bfd5933ef07378
brutal-45 <creatorsports81@gmail.com>
2026-05-24 22:29:29 +0530

    Add files via upload

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,11 +5,11 @@ import { FlatCompat } from '@eslint/eslintrc'
 import { fileURLToPath } from 'node:url'
 import globals from 'globals'
 import path from 'node:path'
-import js from '@eslint/js' 
+import js from '@eslint/js'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
-const compat = new FlatCompat({ 
+const compat = new FlatCompat({
   baseDirectory: __dirname,
   recommendedConfig: js.configs.recommended,
   allConfig: js.configs.all
